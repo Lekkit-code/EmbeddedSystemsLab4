264eff54690135b886c7568005ebad0cbe822572
Kettil Wigenstedt <kettil.brochmann@gmail.com>
2020-11-06 18:59:25 +0100

    Change main.c to write and read two bytes.
    Now writes two bytes to the EEPROM and reads them back.

--- main.c
+++ main.c
@@ -12,15 +12,24 @@
 #include "timer.h"
 
 
+
 int main (void) {
 
 	i2c_init();
 	uart_init();
-
+	char c;
 	sei();
 
 	while (1) {
-
+		eeprom_write_byte(0x10, 'k');
+		eeprom_wait_until_write_complete();
+		eeprom_write_byte(0x11, 'b');
+		eeprom_wait_until_write_complete();
+		c = eeprom_read_byte(0x10);
+		printf("%c", c);
+		c = eeprom_read_byte(0x11);
+		printf("%c", c);
+		_delay_ms(1000);
 	}
 	return 0;
 }
